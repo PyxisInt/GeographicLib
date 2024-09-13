24406f5aae281496daaf93f89d48131fdce91d93
Prabal Ghosh <prabalg@outlook.com>
2024-09-13 12:18:43 -0400

    Fall back to using csproj to set nuget info

--- scripts/Update-NuspecVersion.ps1
+++
@@ -1,9 +0,0 @@
-﻿# Update-NuspecVersion.ps1
-param (
-    [string]$nuspecPath,
-    [string]$newVersion
-)
-
-[xml]$nuspec = Get-Content $nuspecPath
-$nuspec.package.metadata.version = $newVersion
-$nuspec.Save($nuspecPath)
