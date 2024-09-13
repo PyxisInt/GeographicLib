ad90e896c36bc4e20cb5eae8206b718c9dc17d6b
Prabal Ghosh <prabalg@outlook.com>
2024-09-13 12:09:38 -0400

    Added code to change package version in Nuspec file

---
+++ scripts/Update-NuspecVersion.ps1
@@ -0,0 +1,9 @@
+﻿# Update-NuspecVersion.ps1
+param (
+    [string]$nuspecPath,
+    [string]$newVersion
+)
+
+[xml]$nuspec = Get-Content $nuspecPath
+$nuspec.package.metadata.version = $newVersion
+$nuspec.Save($nuspecPath)
